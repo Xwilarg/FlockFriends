043840bd91281fe4a0926f65c9e1693381eff08b
Xwilarg <xwilarg@protonmail.com>
2024-01-05 22:16:47 +0100

    Load birds

--- Assets/Scripts/Level/LevelData.cs
+++ Assets/Scripts/Level/LevelData.cs
@@ -1,15 +1,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TouhouJam.Player;
+using System.Linq;
+using TouhouJam.Manager;
 
 namespace TouhouJam.Level
 {
 	public class LevelData : MonoBehaviour
 	{
-		public List<EBird> availableBirds;
+		[SerializeField]
+		private EBird[] _newBirds;
+
+		public List<EBird> AvailableBirds { private set; get; }
 
 		public static LevelData current;
 
-		void Awake() => current = this;
+		void Awake()
+		{
+            current = this;
+			AvailableBirds = _newBirds.Take(GameManager.NextLevel).Where(x => x != EBird.None).ToList();
+
+			Debug.Log($"[LEV] Loading birds {string.Join(", ", AvailableBirds)}");
+        }
 	}
 }
